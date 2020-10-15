e8ac8f2849e2d31835d7f688161cff70057e66a1
Tim <tim.langner0@gmail.com>
2020-10-15 12:17:57 +0200

    :construction: Show Toast on button click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import './App.scss';
+import React, {useState} from 'react';
+import {Button} from "@material-ui/core";
+import ToastContainer from "./ToastContainer/ToastContainer";
+import './app.scss';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+const App = () => {
+    const [showToast, setShowToast] = useState(true);
+
+    return (
+        <div className="app">
+            {
+                showToast && <ToastContainer />
+            }
+            <Button variant="contained" color="primary" onClick={() => {
+              setShowToast(true);
+            }}>
+              Show Toast!
+            </Button>
+        </div>
+    );
 }
 
 export default App;
