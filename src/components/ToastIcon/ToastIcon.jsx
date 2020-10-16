96d72ca92be908ace5118edee229f7e90af05219
Tim <tim.langner0@gmail.com>
2020-10-16 09:13:55 +0200

    :sparkles: Add the right status-icon based on the type

--- src/components/ToastIcon/ToastIcon.jsx
+++ src/components/ToastIcon/ToastIcon.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import PropTypes from 'prop-types';
 import './toastIcon.scss';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faCheckCircle } from "@fortawesome/pro-light-svg-icons";
+import { faInfoCircle, faCheckCircle, faExclamationCircle, faTimesCircle } from "@fortawesome/pro-light-svg-icons";
 
 const ToastIcon = ({ type }) => {
+    const [iconType, setIconType] = useState(faInfoCircle);
+
+    useEffect(() => {
+        switch (type) {
+            case "info":
+                setIconType(faInfoCircle);
+                break;
+            case "success":
+                setIconType(faCheckCircle);
+                break;
+            case "warning":
+                setIconType(faExclamationCircle);
+                break;
+            case "error":
+                setIconType(faTimesCircle);
+                break;
+            default:
+                setIconType(faInfoCircle);
+        }
+    }, []);
+
     return (
         <div className="icon-wrapper">
-            <FontAwesomeIcon icon={faCheckCircle} className="icon"/>
+            <FontAwesomeIcon icon={iconType} className="icon"/>
         </div>
     );
 };
