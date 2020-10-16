51d264d8b5533e532f90c88b26c466f26fca28ed
Tim <tim.langner0@gmail.com>
2020-10-16 10:11:45 +0200

    :art: Add Toast component which renders the container

---
+++ src/components/Toast/Toast.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import React, {useState} from "react";
+import ToastContainer from "../ToastContainer/ToastContainer";
+import "./toast.scss";
+
+const Toast = ({ type, message, position, setShowToast }) => {
+
+  return (
+    <div className={`toast ${position}`}>
+      <ToastContainer type={type} message={message} position={position} setShowToast={setShowToast} />
+    </div>
+  );
+};
+
+export default Toast;
+
+Toast.propTypes = {
+  type: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  setShowToast: PropTypes.func.isRequired,
+};
