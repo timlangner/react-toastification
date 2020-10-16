fb5b9b99f5bb1b8cf55b07fa3519bb42e40ea1ea
Tim <tim.langner0@gmail.com>
2020-10-16 10:50:36 +0200

    :fire: Remove button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,11 @@
-import { Button } from "@material-ui/core";
-import React, { useState } from 'react';
+import React from 'react';
 import Toast from './Toast/Toast';
 import './app.scss';
 
 const App = () => {
-    const [showToast, setShowToast] = useState(true);
-
     return (
         <div className="app">
-            {
-                showToast && <Toast type="info" message="This is an info message!" position="top-right" setShowToast={setShowToast} />
-            }
-            <Button variant="contained" color="primary" onClick={() => {
-              setShowToast(true);
-            }}>
-              Show Toast!
-            </Button>
+            <Toast type="info" message="This is an info message!" position="top-right" />
         </div>
     );
 }
