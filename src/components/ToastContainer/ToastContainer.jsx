f58c537e754727faafeb26b4fb8c202050512c46
Tim <tim.langner0@gmail.com>
2020-10-16 10:12:49 +0200

    :fire: Remove inline style animation

--- src/components/ToastContainer/ToastContainer.jsx
+++ src/components/ToastContainer/ToastContainer.jsx
@@ -1,21 +1,20 @@
-import React, {useState} from "react";
-import ToastMessage from "../ToastMessage/ToastMessage";
-import ToastIcon from "../ToastIcon/ToastIcon";
-import CloseButton from "../CloseButton/CloseButton";
 import PropTypes from 'prop-types';
+import React from "react";
+import CloseButton from "../CloseButton/CloseButton";
+import ToastIcon from "../ToastIcon/ToastIcon";
+import ToastMessage from "../ToastMessage/ToastMessage";
 import './toastContainer.scss';
 
 const ToastContainer = ({ type, message, setShowToast }) => {
-    const [animation, setAnimation] = useState(0);
 
     return (
-        <div className={`toast-container ${type}`} style={animation === 1 ? { animation: 'bounceOut 1s' } : { animation: 'bounceIn 1s' }}>
+        <div className={`toast-container ${type}`}>
             <div className="content-wrapper">
                 <div className="status-wrapper">
                     <ToastIcon type={type} />
                     <ToastMessage message={message} />
                 </div>
-                <CloseButton setShowToast={setShowToast} setAnimation={setAnimation}/>
+                <CloseButton setShowToast={setShowToast} />
             </div>
         </div>
     );
